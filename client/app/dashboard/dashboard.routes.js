f0dcc910d5a56d6c6d15e9e42d8abbd0554b4d8a
syzer3@gmail.com <syzer3@gmail.com>
2016-10-29 15:31:50 +0200

    fonts, no need to auth

--- client/app/dashboard/dashboard.routes.js
+++ client/app/dashboard/dashboard.routes.js
@@ -8,6 +8,6 @@ export default function routes($stateProvider) {
     template: require('./dashboard.html'),
     controller: 'DashboardController',
     controllerAs: 'vm',
-    authenticate: 'user'
+    // authenticate: 'user'
   });
 }
