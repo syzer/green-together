41573498986b6d9312ff19709730205b124c7488
syzer3@gmail.com <syzer3@gmail.com>
2016-10-29 14:35:58 +0200

    pledge on /login, /login default

--- client/app/account/login/index.js
+++ client/app/account/login/index.js
@@ -1,7 +1,6 @@
-'use strict';
-
-import angular from 'angular';
-import LoginController from './login.controller';
+import angular from 'angular'
+import LoginController from './login.controller'
+import './login.css'
 
 export default angular.module('App.login', [])
   .controller('LoginController', LoginController)
--- client/app/app.config.js
+++ client/app/app.config.js
@@ -3,7 +3,7 @@
 export function routeConfig($urlRouterProvider, $locationProvider) {
   'ngInject';
 
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/login');
 
   $locationProvider.html5Mode(true);
 }
