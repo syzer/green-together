b62603d39d0a4a89817354cc79716782c4cd6dcb
syzer3@gmail.com <syzer3@gmail.com>
2016-11-01 00:07:32 +0100

    small changes to layout

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -67,13 +67,13 @@ export default class SettingsController {
     if (form.$valid) {
       this.Auth.changePassword(this.user.oldPassword, this.user.newPassword)
         .then(() => {
-          this.message = 'Password successfully changed.';
+          this.message = 'Password successfully changed.'
         })
         .catch(() => {
-          form.password.$setValidity('mongoose', false);
-          this.errors.other = 'Incorrect password';
-          this.message = '';
-        });
+          form.password.$setValidity('mongoose', false)
+          this.errors.other = 'Incorrect password'
+          this.message = ''
+        })
     }
   }
 }
